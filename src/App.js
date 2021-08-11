069e93464441dbf5c11c7e9323590ec3ee98aaae
shamolmojumder <shamolmojumder14@gmail.com>
2021-08-12 00:59:19 +0600

    42-2 Create simple login form email and password

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
-import SignUp from './components/SignUp/SignUp';
+import './App.css';
+import firebaseConfig from './firebase.config';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import { useState } from 'react';
+
 function App() {
-  return (
-  <div>
-    <SignUp></SignUp>
+  if (firebase.apps.length===0) {
+    firebase.initializeApp(firebaseConfig); 
+}
+  const [newUser,setNewUser]=useState(false);
+  const [user,setuser]=useState({
     
-  </div>
+  });
+  const handleBlur=(e)=>{
+    console.log(e.target.value,e.target.name);
+  }
+  const handleSubmit=()=>{
+    console.log("submited");
+  }
+  return (
+    <div>
+        <h1>OUR OWN AHUTENTICATION </h1>
+
+        <form onSubmit={handleSubmit} action="">
+        <input type="text" onBlur={handleBlur} name="email" id="" placeholder="email" required/>
+        <br />
+        <input type="password" onBlur={handleBlur} name="password" id="" placeholder="password" required/>
+        <br />
+        <input type="submit" value="Submit" />
+        </form> 
+       
+    </div>    
   );
 }
 
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCSwcagboEUjjFmB3l79JmObe1A6bxTe2w",
+    authDomain: "react-gokada.firebaseapp.com",
+    projectId: "react-gokada",
+    storageBucket: "react-gokada.appspot.com",
+    messagingSenderId: "421818000822",
+    appId: "1:421818000822:web:02452b2ca1969067524d17"
+  };
+
+ export default firebaseConfig;
